6487271d338254aabf4d15279bc3f8eafef5b526
Emil Julian Costinea <ejc@arp-hansen.dk>
2023-05-02 15:52:38 +0200

    more unit tests

--- unit-tests-nunit/ErrorLoggerTests.cs
+++ unit-tests-nunit/ErrorLoggerTests.cs
@@ -14,4 +14,31 @@ public class ErrorLoggerTests
         
         Assert.That(logger.LastError, Is.EqualTo("a"));
     }
+    
+    [Test]
+    [TestCase(null)]
+    [TestCase("")]
+    [TestCase(" ")]
+    public void Log_InvalidError_ThrowArgumentNullException(string error)
+    {
+        var logger = new ErrorLogger();
+        
+        // logger.Log(error);
+        
+        Assert.That(() => logger.Log(error), Throws.ArgumentNullException);
+    }
+    
+    [Test]
+    public void Log_ValidError_RaiseErrorLoggedEvent()
+    {
+        var logger = new ErrorLogger();
+        
+        var id = Guid.Empty;
+        
+        logger.ErrorLogged += (sender, args) => { id = args; };
+        
+        logger.Log("a");
+        
+        Assert.That(id, Is.Not.EqualTo(Guid.Empty));
+    }
 }
---
+++ unit-tests-nunit/FizzBuzzTests.cs
@@ -0,0 +1,17 @@
+﻿namespace unit_tests_nunit;
+
+[TestFixture]
+public class FizzBuzzTests
+{
+    [Test]
+    [TestCase(15, "FizzBuzz")]
+    [TestCase(3, "Fizz")]
+    [TestCase(5, "Buzz")]
+    [TestCase(1, "1")]
+    public void GetOutput_WhenCalled_ReturnsExpectedOutput(int number, string expectedResult)
+    {
+        var result = FizzBuzz.GetOutput(number);
+        
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+}
